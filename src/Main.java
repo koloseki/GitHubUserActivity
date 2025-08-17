8bb5cfde87b0817538dad47627574ab15e172b1b
koloseki <koloseki@proton.me>
2025-08-17 18:56:01 +0200

    Basic Functionalities. Print response from github api to console, catches errors, recognizes when there is no argument.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,40 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        try{
+            HttpClient client = HttpClient.newHttpClient();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            if (args.length < 1) {
+                System.err.println("You dint't provide the username you want to search history off.");
+                return;
+            }
+            String username = args[0];
+
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("https://api.github.com/users/" + username + "/events"))
+                    .header("User-Agent", "GitHubUserActivity/1.0")
+                    .build();
+
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            if (response.statusCode() == 200) {
+                System.out.println(response.body());
+            }else {
+                System.out.println("Something went wrong :(");
+                System.out.println("Error: " + response.statusCode());
+            }
+
+
+
+        }catch (Exception e){
+            e.printStackTrace();
         }
+
     }
 }
